e67669022aa71a4befbbab26e98db6b8f019b478
rolex <zengronghui@gmail.com>
2020-04-11 13:39:23 +0800

    leetcode

---
+++ data-structure/src/main/java/com/rolex/microlabs/leetcode/Test53.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 bsyonline
+ */
+package com.rolex.microlabs.leetcode;
+
+/**
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ *
+ * @author rolex
+ * @since 2020
+ */
+public class Test53 {
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int i = new Test53().maxSubArray(arr);
+        System.out.println(i);
+    }
+}
