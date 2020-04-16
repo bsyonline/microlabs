2cfc236169d23e9be944514cd6dc30b3727b6c1c
rolex <zengronghui@gmail.com>
2020-04-16 23:20:57 +0800

    dubbo

---
+++ dubbo-lab/dubbo-example/dubbo-consumer/src/main/java/com/rolex/microlabs/service/DemoService.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2020 bsyonline
+ */
+package com.rolex.microlabs.service;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+public interface DemoService {
+    String sayHello(String name);
+}
