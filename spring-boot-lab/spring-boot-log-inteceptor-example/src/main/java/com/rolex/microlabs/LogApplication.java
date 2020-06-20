3e6a696379d9f403d52a511e74234559f379ece0
rolex <zengronghui@gmail.com>
2020-06-20 13:04:26 +0800

    spring boot log interceptor using spring aop

---
+++ spring-boot-lab/spring-boot-log-inteceptor-example/src/main/java/com/rolex/microlabs/LogApplication.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2020 bsyonline
+ */
+package com.rolex.microlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+//@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@SpringBootApplication
+public class LogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class, args);
+    }
+}
