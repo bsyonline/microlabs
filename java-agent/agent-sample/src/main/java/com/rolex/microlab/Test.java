a6f75b1c5814a4d8abc4b00337d5fe1928c7cebe
zengronghui <zengronghui@kingsoft.com>
2020-08-30 22:27:23 +0800

    java agent

---
+++ java-agent/agent-sample/src/main/java/com/rolex/microlab/Test.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2020 bsyonline
+ */
+package com.rolex.microlab;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+}
