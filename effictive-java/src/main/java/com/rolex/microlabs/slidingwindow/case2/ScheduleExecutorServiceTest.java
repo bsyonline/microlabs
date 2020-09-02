308bccfcc9c8df33ea57033ac8f3288daa253c8e
rolex <zengronghui@gmail.com>
2020-09-02 22:44:29 +0800

    slid window

---
+++ effictive-java/src/main/java/com/rolex/microlabs/slidingwindow/case2/ScheduleExecutorServiceTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2020 bsyonline
+ */
+package com.rolex.microlabs.slidingwindow.case2;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+public class ScheduleExecutorServiceTest {
+    public static void main(String[] args) {
+
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(LocalDateTime.now().toString());
+            }
+        },3, 5, TimeUnit.SECONDS);
+
+
+    }
+}
