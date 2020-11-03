a12fe01c6b249aa9a547ee17056aed7c60ee9913
rolex <zengronghui@gmail.com>
2020-11-04 07:01:03 +0800

    javassist

---
+++ byte-code/javassist/javassist-sample/src/main/java/com/rolex/microlabs/javassist02/JavassistSample02.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2020 bsyonline
+ */
+package com.rolex.microlabs.javassist02;
+
+import javassist.ClassPool;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+public class JavassistSample02 {
+    public static void main(String[] args) {
+        ClassPool classPool = ClassPool.getDefault();
+
+    }
+}
---
+++ byte-code/javassist/javassist-sample/src/main/java/com/rolex/microlabs/javassist02/JavassistTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2020 bsyonline
+ */
+package com.rolex.microlabs.javassist02;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+public class JavassistTest {
+    private int a = 10;
+
+    public int plus(int i, int j) {
+        return i + j;
+    }
+
+    public double divide(int i, int j) {
+        return i / j;
+    }
+}
