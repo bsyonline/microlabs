9253797f5cae5851e88beda2bcc446e3d851e1e5
zengronghui <zengronghui@kingsoft.com>
2020-11-28 13:48:46 +0800

    builder

---
+++ design-pattern/src/main/java/com/rolex/microlabs/creational/builder/Profession.java
@@ -0,0 +1,14 @@
+package com.rolex.microlabs.creational.builder;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+public enum Profession {
+    Warrior(10), Mage(3), Paladin(10), Hunter(7);
+    int point;
+
+    Profession(int point) {
+        this.point = point;
+    }
+}
