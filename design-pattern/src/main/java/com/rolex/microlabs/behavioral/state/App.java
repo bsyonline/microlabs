5816313c41d78c0dce2427318745eabb07f80fc8
rolex <zengronghui@gmail.com>
2020-12-01 09:28:34 +0800

    observer

--- design-pattern/src/main/java/com/rolex/microlabs/behavioral/state/App.java
+++ design-pattern/src/main/java/com/rolex/microlabs/behavioral/state/App.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2020 bsyonline
  */
-package com.rolex.microlabs.creational.prototype;
+package com.rolex.microlabs.behavioral.state;
 
 /**
  * @author rolex
  * @since 2020
  */
-public interface Car {
+public class App {
 }
--- design-pattern/src/main/java/com/rolex/microlabs/structural/proxy/dynamic/Advice.java
+++ design-pattern/src/main/java/com/rolex/microlabs/structural/proxy/dynamic/Advice.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright (C) 2020 bsyonline
+ */
 package com.rolex.microlabs.structural.proxy.dynamic;
 
 /**
