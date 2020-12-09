8be0354e240cbadf512d2f6339f2c9442138d756
zengronghui <zengronghui@kingsoft.com>
2020-12-09 09:07:42 +0800

    strategy

---
+++ design-pattern/src/main/java/com/rolex/microlabs/behavioral/strategy/BlackCardStrategy.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2020 bsyonline
+ */
+package com.rolex.microlabs.behavioral.strategy;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+public class BlackCardStrategy implements RateStrategy {
+    @Override
+    public double fee(double price) {
+        return price * 0.8;
+    }
+}
---
+++ design-pattern/src/main/java/com/rolex/microlabs/behavioral/strategy/RateStrategy.java
@@ -0,0 +1,11 @@
+package com.rolex.microlabs.behavioral.strategy;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+public interface RateStrategy {
+
+    double fee(double price);
+
+}
