401123c36516cabca9fae07289d73bb0b86319a0
rolex <zengronghui@gmail.com>
2021-01-09 19:47:14 +0800

    spring boot websocket

--- spring-boot/spring-boot-websocket/src/main/java/com/rolex/microlabs/config/WebSocketConfig.java
+++ spring-boot/spring-boot-websocket/src/main/java/com/rolex/microlabs/config/WebSocketConfig.java
@@ -3,29 +3,18 @@
  */
 package com.rolex.microlabs.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * @author rolex
  * @since 2021
  */
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry
-                                               registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("mydomain.com").withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic/", "/queue/");
-        config.setApplicationDestinationPrefixes("/app");
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 }
