093da7f93fd2821ead7d3e5085da8e40f0e50d19
zengronghui <zengronghui@kingsoft.com>
2021-01-30 08:52:24 +0800

    spring security oauth2

---
+++ spring-security/spring-security-example/src/main/java/com/rolex/alphax/controller/ProductController.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2020 bsyonline
+ */
+package com.rolex.alphax.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @GetMapping
+    public String list() {
+        return "product list";
+    }
+
+}
---
+++ spring-security/spring-security-example/src/main/java/com/rolex/alphax/dao/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.rolex.alphax.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rolex.alphax.dao.entity.User;
+
+/**
+ * @author rolex
+ * @since 2020
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
